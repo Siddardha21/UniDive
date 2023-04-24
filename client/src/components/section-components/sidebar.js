1b37603ea444523dfff6810393d9dbf4baaf2dde
Yaswanth Krishna Karuturi <yk@Yaswanths-MacBook-Air.local>
2023-04-24 11:19:07 -0400

    new changes

--- client/src/components/section-components/sidebar.js
+++ client/src/components/section-components/sidebar.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {SidebarData} from './sidebardata';
 
 function Sidebar() {
+
+    const[universities, setUniversities] = useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:8080/uniDive/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+            setUniversities(result);
+        })
+    },[])
+
   return (
     <div className='sidebar'>
+        {universities.map((data, key) => (
+            <div key={data.id}>
+                {data.uniname}
+                {data.address}
+            </div>
+        ))}
         <div className='side'>
             <ul className='stylesidebar'>
                 { SidebarData.map((value, key) => {
@@ -11,7 +28,6 @@ function Sidebar() {
                         <li key = {key} className='innerlist'
                         id={window.location.pathname === value.link ? "inState" : ""}
                         onClick={() => {window.location.pathname = value.link;}}>
-                            
                             <div id='icon'>{value.icon}</div>
                             {" "}
                             <div id='title'>
